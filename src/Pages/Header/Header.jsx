0b98c44573c7959a20c8a77d526b17fe1de17b43
naeem <naeemislamjr182@gmail.com>
2023-10-03 22:06:35 +0600

    basic layout

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,9 +1,11 @@
-
-
+import logo from '../../assets/asset/logo.png'
+import moment from 'moment';
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className=' items-center text-center mx-auto justify-center'>
+            <img className=' items-center text-center mx-auto justify-center' src={logo} alt="" />
+            <p className=' my-2 font-poppins text-md'>Journalism Without Fear or Favour</p>
+            <h2 className=' text-xl'>{moment().format("dddd, MMMM D, YYYY")}</h2>
         </div>
     )
 }

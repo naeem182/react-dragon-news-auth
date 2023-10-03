2baf705183fb4a754f192799e583336fc9a5f919
naeem <naeemislamjr182@gmail.com>
2023-10-03 22:32:07 +0600

    Header breaking newa

---
+++ src/BreakingNews.jsx
@@ -0,0 +1,18 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+const BreakingNews = () => {
+    return (
+        <div className=" flex">
+            <button className=" btn btn-secondary">Latest</button>
+            <Marquee pauseOnHover={true} speed={90}>
+                <Link to={''}>I can be a React component, multiple React components</Link>
+                <Link className=" mx-6" to={''}>I can be a React component, multiple React components</Link>
+                <Link to={''}>I can be a React component, multiple React components</Link>
+            </Marquee>
+
+        </div>
+    )
+}
+
+export default BreakingNews
--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,12 +1,18 @@
+import BreakingNews from '../../BreakingNews';
 import logo from '../../assets/asset/logo.png'
 import moment from 'moment';
 const Header = () => {
     return (
-        <div className=' items-center text-center mx-auto justify-center'>
-            <img className=' items-center text-center mx-auto justify-center' src={logo} alt="" />
-            <p className=' my-2 font-poppins text-md'>Journalism Without Fear or Favour</p>
-            <h2 className=' text-xl'>{moment().format("dddd, MMMM D, YYYY")}</h2>
-        </div>
+        <>
+            <div className='  text-center mx-auto justify-center'>
+                <img className=' mt-3 mx-auto' src={logo} alt="" />
+                <p className=' my-2 font-poppins text-md'>Journalism Without Fear or Favour</p>
+                <h2 className=' text-xl'>{moment().format("dddd, MMMM D, YYYY")}</h2>
+            </div>
+            <div>
+                <BreakingNews></BreakingNews>
+            </div>
+        </>
     )
 }
 

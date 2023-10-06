b279dfb1b58f4765a84e7daaca82e0cebe8e43b5
naeem <naeemislamjr182@gmail.com>
2023-10-06 12:37:22 +0600

    news details

--- src/Pages/NewsPages/NewsDetails.jsx
+++ src/Pages/NewsPages/NewsDetails.jsx
@@ -1,12 +1,28 @@
+import { useParams } from "react-router-dom";
+import Header from "../Header/Header";
+import Navbar from "../../Navbar/Navbar";
+import RightsightNav from "../../Navbar/RightsightNav";
 
 
 const NewsDetails = () => {
-    // const { id } = useParams();
+    const { id } = useParams();
     return (
         <div>
-            details
+            <Header></Header>
+            <Navbar></Navbar>
+            <div className="grid md:grid-cols-4">
+                <div className="col-span-3">
+                    <h2 className="text-5xl">News Detail</h2>
+                    <p>{id}</p>
+
+                </div>
+                <div>
+                    <RightsightNav></RightsightNav>
+                </div>
+            </div>
+
         </div>
-    )
-}
+    );
+};
 
 export default NewsDetails
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>,
-                loader: () => fetch('news.json')
+                loader: () => fetch('/news.json')
             },
             {
                 path: "/news/:id",
